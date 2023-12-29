00bb514627cdf50035ef085a64677241939b07f7
Migayeon <edisonjini@naver.com>
2023-12-29 19:35:38 +0900

    test3
    add scene2

---
+++ Project_Zero/Assets/Scripts/Scene2Test.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene2Test : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("We are in Scene2!");
+    }
+}
