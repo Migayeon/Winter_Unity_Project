ace0f8247726da2d68ef9eff36f83e809bcaadf3
Migayeon <edisonjini@naver.com>
2023-12-29 18:54:23 +0900

    test1
    1

---
+++ Project_Zero/Assets/Scripts/Scene1Test.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scene1Test : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("Scene1Test!");
+    }
+}
