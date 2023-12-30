59d9a2a2211fe966e1af96958b92677d39e9120e
Migayeon <edisonjini@naver.com>
2023-12-30 16:55:36 +0900

    test6
    hehe

---
+++ Project_Zero/Assets/Scripts/dog.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dog : MonoBehaviour
+{
+    void Update()
+    {
+        Debug.Log("Bow");       
+    }
+}
