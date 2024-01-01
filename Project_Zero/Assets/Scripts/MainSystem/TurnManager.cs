88ea93e04b49e6008cfc4a5893ab561ecce86e94
Migayeon <edisonjini@naver.com>
2024-01-01 14:52:14 +0900

    Update 1

---
+++ Project_Zero/Assets/Scripts/MainSystem/TurnManager.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ 
+
+
+ 
+ 
+ */
+
+
+public class TurnManager : MonoBehaviour
+{
+    public static int turn;
+
+    public void NewGame()
+    {
+        turn = 0;
+
+        
+    }
+}
