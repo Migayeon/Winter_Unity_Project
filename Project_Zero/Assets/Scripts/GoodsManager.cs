6679ff89347e41b3419e6806ed382b7d66a3297d
BOOK-BTMG0R2LRL\82107 <chanhyng001@gmail.com>
2024-01-01 21:43:35 +0900

    Revert "턴 시스템 구현, 마정석 주식 시스템 구현"
    This reverts commit a20acd12f42cd03898f1f5572cf8b761265c926e.

--- Project_Zero/Assets/Scripts/GoodsManager.cs
+++ Project_Zero/Assets/Scripts/GoodsManager.cs
@@ -4,21 +4,24 @@ using UnityEngine;
 
 public class GoodsManager : MonoBehaviour
 {
-    // 기본 재화
-    public static int goodsAr = 0;
-    public static int goodsStone = 0;
-    public static int goodsFame = 0;
-    public static int goodsStudent = 0;
-
-    // 아르<->마정석 환전
-    public static int exchangePercent = 0;
-    public static int exchangeRate = 500;
-    public static int maxRate = 20;
-    public static int minRate = -20;
-
+    private int goodsAr { get; set; }
+    private int goodsStone { get; set; }
+    private int goodsFame { get; set; }
+    private int goodsStudent { get; set; }
     void Start()
     {
-        // json 저장 시스템 구현되면 try ~ except문으로 각 재화 초기화
+        goodsAr = 0;
+        goodsStone = 0;
+        goodsFame = 0;
+        goodsStudent = 0;
+        // json 구현되면 try ~ except문으로 각 재화 초기화
+    }
+    public void GoodsChange(string goodsType, int rate)
+    {
+        if (goodsType == "Ar") goodsAr += rate;
+        if (goodsType=="Stone") goodsStone += rate;
+        if (goodsType=="Fame") goodsFame += rate;
+        if (goodsType=="Student") goodsStudent += rate;
     }
     public static void UpdateUI()
     {
--- Project_Zero/Assets/Scripts/StocksManager.cs
+++ Project_Zero/Assets/Scripts/StocksManager.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class StocksManager : MonoBehaviour
 {
+    // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    // Update is called once per frame
     void Update()
     {
         
