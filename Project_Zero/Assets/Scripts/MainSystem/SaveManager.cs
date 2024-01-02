ff4a8937a8dbb265e4b63b6c039594f80a89ff50
Migayeon <edisonjini@naver.com>
2024-01-02 21:35:19 +0900

    Save 3
    세이브 시스템 구현중.
    현재 교수 구조체 string으로 저장하는 것 완성. ,(comma) 로 구분하므로 내일 parsing을 통해 불러오기 까지 구현할 것.

--- Project_Zero/Assets/Scripts/MainSystem/SaveManager.cs
+++ Project_Zero/Assets/Scripts/MainSystem/SaveManager.cs
@@ -1,22 +1,26 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using Unity.VisualScripting;
 using UnityEngine;
 
 [System.Serializable]
 public class SaveData
 {
-    public SaveData() 
+    public SaveData(int _t, int _a, int _s, int _sN, int _f) 
     {
-        
+        turn = _t;
+        ar = _a;
+        stone = _s;
+        studentsNum = _sN;
+        fame = _f;
     }
-
-    public int turn;
-    public string name;
-    public int ar;
-    public int debt;
-    public string[] tc;
-
+    public SaveData() { }
+    public int turn, ar, stone, studentsNum, fame; //, debt; 빚 추가해야 함.
+    //public string myName, arcademyName;
+    public string[] professor; // 클래스 정보 받아서 하나의 스트링으로
+    public string[] students;
     /*
      
     재화 시스템에 따라 더 추가하기. (무엇을 저장해야 하는가?) 
@@ -24,15 +28,10 @@ public class SaveData
     */
 }
 
-public class testClass
-{
-    public string name;
-    public int id;
-}
-
 public class SaveManager : MonoBehaviour
 {
-    public static SaveData LoadData(int i)
+    private static ProfessorSystem.Professor professor1;
+    public static SaveData LoadProcess(int i)
     {
         if (PlayerPrefs.HasKey("save"+i.ToString()))
         {
@@ -43,13 +42,56 @@ public class SaveManager : MonoBehaviour
         return saveData;
     }
 
+    public static void SaveProcess(int i)
+    {
+        SaveData newSave = new SaveData();
+        newSave.turn = TurnManager.turn;
+        newSave.ar = GoodsManager.goodsAr;
+        newSave.stone = GoodsManager.goodsStone;
+        newSave.fame = GoodsManager.goodsFame;
+        newSave.studentsNum = GoodsManager.goodsStudent;
+
+        //newSave.myName =
+        //newSave.Arcademy = 
+        newSave.professor = new string[10];
+        newSave.professor[0] = SaveProfessor(professor1);
+        Debug.Log(SaveProfessor(professor1));
+        string json = JsonUtility.ToJson(newSave, true);
+        File.WriteAllText($"Assets\\Resources\\Data\\save{i}.json",json);
+        Debug.Log("Success");
+    }
+
+    private static string SaveProfessor(ProfessorSystem.Professor professor)
+    {
+        string information = "";
+        information += professor.ProfessorGetID().ToString() + ',';
+        information += professor.ProfessorGetName() + ',';
+        information += professor.ProfessorGetTenure().ToString() + ',';
+        information += professor.ProfessorGetType().ToString() + ',';
+        List<int> stat = professor.ProfessorGetStats();
+        foreach (int i in stat)
+            information += i.ToString() + ',';
+        information += professor.ProfessorGetSalary() + ',';
+        information += professor.ProfessorGetAwayStatus() ? "1" : "0";
+
+        return information;
+    }
+
     private void Awake()
     {
+        List<int> stat = new List<int>{1, 2, 3, 4, 5, 6 };
+        professor1 = new ProfessorSystem.Professor(1, "장형수", 10, 1, stat);
+        
+        /*
         SaveData save = LoadData(1);
         Debug.Log(save.name);
         Debug.Log(save.turn);
         Debug.Log(save.tc[0]);
         Debug.Log(save.tc[1]);
+        */
+        SaveProcess(1);
+        SaveData saved = LoadProcess(1);
+        //Debug.Log(saved.stat[0]);
     }
 }
 
