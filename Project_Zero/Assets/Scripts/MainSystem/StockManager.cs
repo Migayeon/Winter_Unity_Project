dc08393001971b35e1341bd56bb957005002ad6d
CH-HY <131160830+CH-HY@users.noreply.github.com>
2024-01-02 13:38:58 +0900

    DDD

--- Project_Zero/Assets/Scripts/MainSystem/StockManager.cs
+++ Project_Zero/Assets/Scripts/MainSystem/StockManager.cs
@@ -3,16 +3,26 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StockManager : MonoBehaviour
 {
     // 마정석 환전과 관련된 스크립트
+    int purchaseAmount = 0;
+    int saleAmount = 0;
+    // 환전 정보
     [SerializeField] TextMeshProUGUI exchangeRate;
     [SerializeField] TextMeshProUGUI exchangePercent;
+    [SerializeField] TextMeshProUGUI purchaseAmountText;
 
+    // 구매 시스템
+    [SerializeField] Button purchaseButton;
+    [SerializeField] GameObject checkPurchase;
+    [SerializeField] Button checkPurchaseYes;
+    [SerializeField] Button checkPurchaseNo;
     void Awake()
     {
-        
+        purchaseButton.onClick.AddListener(() => AbleUI(checkPurchase));
     }
     void Start()
     {
@@ -21,20 +31,46 @@ public class StockManager : MonoBehaviour
     }
     private void Update()
     {
+        try
+        {
+            purchaseAmountText.text = purchaseAmount.ToString();
+
+        }
+        catch
+        {
+
+        }
 
     }
-    public void MoveToAfterTurn()
+    public void AbleUI(GameObject target)
     {
-        SceneManager.LoadScene("AfterTurn");
+        target.SetActive(true);
     }
-
+    public void DisableUI(GameObject target)
+    {
+        target.SetActive(false);
+    }
     // 구입 관련 함수
-    public void PurchaseStone(int amount)
+    public void PurchaseStone()
     {
-        GoodsManager.goodsAr -= GoodsManager.exchangeRate * amount;
-        GoodsManager.goodsStone += amount;
+        GoodsManager.goodsAr -= GoodsManager.exchangeRate * purchaseAmount;
+        GoodsManager.goodsStone += purchaseAmount;
     }
-
+    public void AddPurchaseAmount(int amount)
+    {
+        if (purchaseAmount + amount < 0)
+        {
+            purchaseAmount = 0;
+        }
+        else if (purchaseAmount + amount > GoodsManager.goodsAr / GoodsManager.exchangeRate) 
+        {
+            purchaseAmount = GoodsManager.goodsAr / GoodsManager.exchangeRate;
+        }
+        else
+        {
+            purchaseAmount += amount;
+        }
+    }
     // 판매 관련 함수
     public void SaleStone()
     {
