417680feb856cd6584767faec8a48520154277e3
Sihoo Park <87002707+sourceds@users.noreply.github.com>
2024-01-02 20:25:28 +0900

    (WIP) CreateProfessor.cs

--- Project_Zero/Assets/Scripts/CreateProfessor.cs
+++ Project_Zero/Assets/Scripts/CreateProfessor.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class CreateProfessor : ProfessorSystem
 {
     const int UniqueProfessorRarity = 5;
+    const int TotalRarity = 100;
     public static string GenerateName()
     {
         StreamReader ReadEnglishName = new StreamReader("Assets\\Resources\\Names\\englishNames.csv");
@@ -52,50 +53,94 @@ public class CreateProfessor : ProfessorSystem
         RandomFirstName = firstName[randomFirstNameIndex];
 
         finalRandomName = RandomLastName + " " + RandomFirstName;
-        Debug.Log(finalRandomName);
+        
+        //log for debug
+        Debug.Log(string.Format("New random name created: {0}", finalRandomName));
         return finalRandomName;
     }
 
     Professor CreateNewProfessor(int iteration)
     {
-        Professor NewProfessor = new Professor();
         System.Random RandomGenerator = new System.Random();
-        List<int> ProfessorRarityList = new List<int>(100);
+        List<int> ProfessorRarityList = new List<int>(TotalRarity);
         for (int i = 0; i < UniqueProfessorRarity; ++i)
         {
-            ProfessorRarityList[i] = 1;
+            ProfessorRarityList.Add(1);
         }
-        for (int i = UniqueProfessorRarity; i < 100; ++i)
+        for (int i = UniqueProfessorRarity; i < TotalRarity; ++i)
         {
-            ProfessorRarityList[i] = 0;
+            ProfessorRarityList.Add(0);
         }
 
-        int ProfessorID = Convert.ToInt32(DateTime.Now.ToString("yyyyMMdd") + DateTime.Now.ToString("HHmmss")) + iteration;
+        long ProfessorID = Convert.ToInt64(DateTime.Now.ToString("yyyyMMdd") + DateTime.Now.ToString("HHmmss")) + (long)iteration;
         string ProfessorName = GenerateName();
         int ProfessorTenure = 0;
         int ProfessorRarity = ProfessorRarityList[RandomGenerator.Next(0, 100)];
+        List<int> ProfessorStats = new List<int>(6);
         if (ProfessorRarity == 1)
         {
-            //insert
+            for (int i = 0; i < ProfessorSystem.professorStats; ++i)
+            {
+                ProfessorStats.Add(666);  //temporary placeholder number, edit as needed
+            }
         }
         else
         {
+            int StatScale = RandomGenerator.Next(3000, 4001);
+            int StatSum = 0;
+            for (int i = 0; i < ProfessorSystem.professorStats; ++i)
+            {
+                ProfessorStats.Add(RandomGenerator.Next(1000, 10000));
+                StatSum += ProfessorStats[i];
+            }
+            for (int i = 0; i < ProfessorSystem.professorStats; ++i)
+            {
+                ProfessorStats[i] = (int)((float)(ProfessorStats[i]) * ((float)StatScale / StatSum));
+            }
             //3000~4000, divide among 6 stats;
-            
         }
         //set salary (sum of all stats)
-        //insert above calculated values into new Professor class object
-        //return object
-        Debug.Log(NewProfessor);
+        int ProfessorSalary = 0;
+        for (int i = 0; i < ProfessorSystem.professorStats; ++i)
+        {
+            ProfessorSalary += ProfessorStats[i];
+        }
+        Professor NewProfessor = new Professor(ProfessorID, ProfessorName, ProfessorTenure, ProfessorRarity, ProfessorStats, ProfessorSalary);
+        Debug.Log(string.Format("Iteration {0}", iteration));
+        NewProfessor.UnityDebugLogProfessorInfo();
+        //test logging
+        /*
+        Debug.Log(string.Format("Iteration {0}", iteration));
+        Debug.Log("Created new professor");
+        Debug.Log(string.Format("Professor ID : {0}", ProfessorID));
+        Debug.Log(string.Format("Professor Name : {0}", ProfessorName));
+        Debug.Log(string.Format("Professor Tenure : {0}", ProfessorTenure));
+        if (ProfessorRarity == 1)
+            Debug.Log("Professor Rarity : unique");
+        else
+            Debug.Log("Professor Rarity : normal");
+        string temp = "Professor Stats : ";
+        for (int i = 0; i < ProfessorSystem.professorStats; ++i)
+        {
+            temp += ProfessorSystem.Professor.statlist[i];
+            temp += " ";
+            temp += Convert.ToString(ProfessorStats[i]);
+            temp += "     ";
+        }
+        Debug.Log(temp);
+        Debug.Log(ProfessorSalary);
+        */
         return NewProfessor;
 
     }
     void Start()
     {
-        List<Professor> NewProfessors = new List<Professor>(3);
+        List<Professor> NewProfessorList = new List<Professor>(3);
+        Professor tempProfessor;
         for (int i = 0; i < 3; ++i)
         {
-            NewProfessors.Add(CreateNewProfessor(i));
+            tempProfessor = CreateNewProfessor(i);
+            NewProfessorList.Add(tempProfessor);
         }
     }
 }
