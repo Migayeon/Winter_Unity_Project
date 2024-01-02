dad1ca3f842f9233a2ff16f05a83c06b42997f93
Migayeon <edisonjini@naver.com>
2024-01-02 12:48:38 +0900

    BGM volume Control
    브금 음량 조절 스크립트를 완성했습니다.

--- Project_Zero/Assets/Scripts/MainSystem/SoundManager.cs
+++ Project_Zero/Assets/Scripts/MainSystem/SoundManager.cs
@@ -10,14 +10,19 @@ public class SoundManager : MonoBehaviour
     public AudioMixer mixer;
     public Slider bgmSlider;
     // Start is called before the first frame update
+    public void ControlBGM()
+    {
+        bgmVolume = bgmSlider.value;
+        if (bgmSlider.value == -40f)
+            mixer.SetFloat("BGM", -80f);
+        else
+            mixer.SetFloat("BGM", bgmVolume);
+       
+    }
 
     private void Awake()
     {
-        //bgmSlider.onValueChanged
+        bgmSlider.onValueChanged.AddListener(delegate { ControlBGM(); });
+        mixer.SetFloat("BGM", bgmSlider.value);
     }
-    public void ControlBGM()
-    {
-
-    }
-
 }
