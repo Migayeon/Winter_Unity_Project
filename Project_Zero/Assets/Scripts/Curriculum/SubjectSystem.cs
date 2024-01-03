3ceb23cf3e82574510949294ddfb4d71a449e7db
CH-HY <131160830+CH-HY@users.noreply.github.com>
2024-01-03 16:06:31 +0900

    살려줘

--- Project_Zero/Assets/Scripts/Curriculum/SubjectSystem.cs
+++ Project_Zero/Assets/Scripts/Curriculum/SubjectSystem.cs
@@ -9,19 +9,19 @@ using UnityEngine;
 public class SubjectSystem : MonoBehaviour
 {
     public const int subjectStats = 5;
-    public const string fileName = "Assets\\Resources\\Subjects//Subject.csv";
+    public const string fileName = "Assets\\Resources\\Subjects\\Subject.csv";
     public class Subject
     {
-        private int id;
-        private long tier;
-        private string subjectName;
-        private List<string> enforceStatType;
-        private List<int> enforceStatAmount;
-        private List<int> nextAvailableID;
-        private int needToBeAvailable;  // 해금해야 하는 선행 연구 수
-        private bool isAvailable;
-        private ProfessorSystem.Professor manager;  // 담당 교수
-        private Dictionary<int, string> enforceStatList = new Dictionary<int, string>()
+        public int id;
+        public long tier;
+        public string subjectName;
+        public List<string> enforceStatType;
+        public List<int> enforceStatAmount;
+        public List<int> nextAvailableID;
+        public int needToBeAvailable;  // 해금해야 하는 선행 연구 수
+        public bool isAvailable;
+        public ProfessorSystem.Professor manager;  // 담당 교수
+        public Dictionary<int, string> enforceStatList = new Dictionary<int, string>()
         {
             {0, "theory"},
             {1, "mana"},
@@ -43,6 +43,10 @@ public class SubjectSystem : MonoBehaviour
             this.needToBeAvailable = needToBeAvailable;
             this.isAvailable = false;
             manager = new ProfessorSystem.Professor();
+            enforceStatAmount = new List<int>()
+            {
+                0,0,0,0,0
+            };
         }
         public void SetEnforceAmount(List<string> type, List<int> amounts)
         {
@@ -52,47 +56,12 @@ public class SubjectSystem : MonoBehaviour
             }
         }
         public void TurnAvailable() { isAvailable = true; }
-    } 
+    }
+
+    /*
     public static List<Subject> ReadSubject()
     {
-        List<Subject> returnSubjects = new List<Subject>();
-        StreamReader sr = new StreamReader(fileName);
-        string line = "";
-        string[] row;
-        sr.ReadLine();
-        while ((line = sr.ReadLine()) != null)
-        {
-            int id;
-            int tier;
-            string name;
-            List<string> type = new List<string>();
-            List<int> amount = new List<int>();
-            List<int> nextID = new List<int>();
-            int need;
 
-            string[] temp;
-            row = line.Split(',');
-            id = Convert.ToInt32(row[0]);
-            tier = Convert.ToInt32(row[1]);
-            name = row[2];
-            type = row[3].Split('+').ToList<string>();
-            temp = row[4].Split('+');
-            for (int i = 0; i < temp.Length; i++) 
-            {
-                amount.Append(Convert.ToInt32(temp[i]));
-            }
-            temp = row[5].Split("+");
-            for (int i=0; i < temp.Length; i++)
-            {
-                nextID.Append(Convert.ToInt32(temp[i]));
-            }
-            need = Convert.ToInt32(row[6]);
-
-            Subject subject = new Subject(id, tier, name, nextID, need);
-            subject.SetEnforceAmount(type, amount);
-            returnSubjects.Append(subject);
-        }
-        sr.Close();
-        return returnSubjects;
     }
+    */
 }
