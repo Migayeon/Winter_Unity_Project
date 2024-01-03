b0d5605d863d1546de69fc6bdb31787bcc70d852
Migayeon <edisonjini@naver.com>
2024-01-03 19:50:50 +0900

    Add StudentGroup Class
    Yeah!!!!!!!!!!11

---
+++ Project_Zero/Assets/Scripts/MainSystem/StatisticsInfo.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatisticsInfo
+{
+    public class StudentStatistics
+    {
+        int period;
+        int studentNum;
+        int cost;
+        int totalCost;
+    }
+
+    public static List<StudentStatistics> studentStatistics;
+    
+}
