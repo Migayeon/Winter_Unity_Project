0208e3170bbc537be9bed15667288496f0aee4af
happylesserpanda <132274562+happylesserpanda@users.noreply.github.com>
2024-01-06 02:33:23 +0900

    학원관리 가격 스태틱화

--- Project_Zero/Assets/Scripts/GUI_Script/ACMA/ClassEx.cs
+++ Project_Zero/Assets/Scripts/GUI_Script/ACMA/ClassEx.cs
@@ -6,31 +6,75 @@ using UnityEngine.UI;
 
 public class ClassEx : MonoBehaviour
 {
-    public GameObject[] managementButton = new GameObject[4];
 
     public Button ClassExp;
     public Button OfficeExp;
     public Button marketingA;
     public Button marketingB;
+    public Text requiredClass;
+    public Text requiredProfessor;
+    public Text requiredMarketA;
+    public Text requiredMarketB;
+
+
+    static private int classAr = 5000;
+    static private int officeAr = 5000;
+
 
     public void ClassExpan()
     {
-        int Ar = 5000;
-        if(GoodsManager.goodsAr - Ar < 0)
+        int num = 50;
+        
+        if(GoodsManager.goodsAr - classAr < 0)
         {
             Debug.Log("파산");
         }
         else
         {
-            GoodsManager.goodsAr -= Ar;
+            GoodsManager.goodsAr -= classAr;
+            classAr *= 2;
+            PlayerInfo.maxStudent += num;
+            requiredClass.text = $"{classAr}";
         }
+    }
 
+    public void OfficeExpan()
+    {
+        
+        int num = 50;
+        if(GoodsManager.goodsAr - officeAr < 0)
+        {
+            Debug.Log("파산");
+        }
+        else
+        {
+            
+            GoodsManager.goodsAr -= officeAr;
+            PlayerInfo.maxProfessor += num;
+            officeAr *= 2;
+            requiredProfessor.text = $"{officeAr}";
+        }
 
     }
 
+    public void marketA()
+    {
+
+
+    }
+    public void marketB()
+    {
+
+    }
+
+
     private void Awake()
     {
-        //ClassExp.onClick.AddListener();
+        requiredClass.text = $"{classAr}";
+        requiredProfessor.text = $"{officeAr}";
+        ClassExp.onClick.AddListener(ClassExpan);
+        
+        OfficeExp.onClick.AddListener(OfficeExpan);
     }
 
 
