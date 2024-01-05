fc910ba2950c2f44b65a67cd51fa315e65738832
Rio Choi <architekt140@gmail.com>
2024-01-05 19:24:28 +0900

    asdf
    asdf

---
+++ Project_Zero/Assets/Resources/UI/Test_Section/kimtest.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+
+class info
+{
+    public int testclass;
+    public string testname;
+    public List<int> require = new List<int>();
+}
+
+public class kimtest : MonoBehaviour
+{
+    public GameObject testcase;
+    public Transform content;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Instantiate(prefab);
+        for (int i = 1; i < 16; i++)
+        {
+            var loadedJson = Resources.Load<TextAsset>("TestCase/" + i.ToString());
+            info testInfo = JsonUtility.FromJson<info>(loadedJson.ToString());
+            Debug.Log($"{testInfo.testclass}, {testInfo.testname}, {testInfo.require}");
+            GameObject test = Instantiate(testcase, content);
+            test.transform.GetChild(1).GetComponent<TextMeshProUGUI>().text = testInfo.testname;
+        }
+
+    }
+}
---
+++ Project_Zero/Assets/Scripts/Student/Testdata_loader.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Testdata_loader : MonoBehaviour
+{
+    public int stageIndex;
+    public TextMeshProUGUI TxtStageName;
+
+}
