9c8837f53a2a1b3453f7100c30e37adefd0ba686
mini2317 <ekdmsdlfma@gmail.com>
2024-01-13 15:56:23 +0900

    credit

---
+++ Project_Zero/Assets/Scripts/GUI_Script/CreditTextUP.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CreditTextUP : MonoBehaviour
+{
+    float speed = 0.01f;
+    void FixedUpdate()
+    {
+        transform.position = transform.position + new Vector3(0f, speed, 0f);
+        if (transform.localPosition.y >= 2700)
+        {
+            SceneManager.LoadScene("Title");
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            speed = 0.3f;
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            speed = 0.01f;
+        }
+    }
+}
