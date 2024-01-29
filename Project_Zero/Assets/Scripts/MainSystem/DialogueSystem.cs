06b293765c5cacdfa133863fc31815be37b52a8a
Migayeon <edisonjini@naver.com>
2024-01-29 16:00:21 +0900

    대화 시스템 완성
    :+1:

--- Project_Zero/Assets/Scripts/MainSystem/DialogueSystem.cs
+++ Project_Zero/Assets/Scripts/MainSystem/DialogueSystem.cs
@@ -1,19 +1,82 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DialogueSystem : MonoBehaviour
 {
-    public static string situation = null;
-    public GameObject dialogue;
-    
+    public static string situation = "Tutorial";// = null;
+    public GameObject dialogueUI;
+    public Button dialogueButton;
+    public Text character;
+    public Text message;
+
+    struct content
+    {
+        public string[] message;
+    }
+
+    private content dialogue;
+    private int dialogueLength;
+    private int dialogueIndex;
+    private string[] dialogueString;
+
+    private bool isNowAnimation;
+
+    IEnumerator DialogueAnimation(string str)
+    {
+        foreach (char letter in str)
+        {
+            message.text += letter;
+            yield return new WaitForSeconds(0.1f);
+        }
+        isNowAnimation = false;
+    }
+
+    private void DialogueProcess()
+    {
+        if(isNowAnimation)
+        {
+            StopAllCoroutines();
+            message.text = dialogueString[1];
+            isNowAnimation = false;
+        }
+        else
+        {
+            if (dialogueIndex == dialogueLength)
+            {
+                CloseDialogue();
+                return;
+            }
+            isNowAnimation = true;
+            message.text = string.Empty;
+
+            dialogueString = dialogue.message[dialogueIndex].Split(":");
+            character.text = dialogueString[0];
+            StartCoroutine(DialogueAnimation(dialogueString[1]));
+            dialogueIndex++;
+        }
+    }
+
+    private void CloseDialogue()
+    {
+        dialogueUI.SetActive(false);
+        dialogueButton.onClick.RemoveAllListeners();
+        situation = null;
+    }
+
     private void Awake()
     {
-        dialogue.SetActive(false);
-        if(situation != null)
+        if (situation != null)
         {
-            dialogue.SetActive(true);
-            
+            TextAsset json = Resources.Load<TextAsset>("Dialogue/" + situation);
+            dialogue = JsonUtility.FromJson<content>(json.ToString());
+            dialogueLength = dialogue.message.Length;
+            dialogueIndex = 0;
+            isNowAnimation = false;
+            dialogueUI.SetActive(true);
+            dialogueButton.onClick.AddListener(DialogueProcess);
+            DialogueProcess();
         }
     }
 }
