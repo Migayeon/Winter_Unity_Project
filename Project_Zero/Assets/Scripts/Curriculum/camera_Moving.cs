66f45392bfd70018eb52bfac706cd7dfe8cfe596
mini2317 <ekdmsdlfma@gmail.com>
2024-01-29 15:02:06 +0900

    우클릭 스크롤

--- Project_Zero/Assets/Scripts/Curriculum/camera_Moving.cs
+++ Project_Zero/Assets/Scripts/Curriculum/camera_Moving.cs
@@ -3,7 +3,6 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public static bool canMove = true;
-    private bool isAlt;
     private Vector2 curPoint;
     private float dragSpeed = 0.01f;
     private Vector2 prevPoint;
@@ -12,39 +11,24 @@ public class CameraController : MonoBehaviour
     {
         if (canMove)
         {
-            if (Input.GetKeyDown(KeyCode.LeftAlt))
-            {
-                isAlt = true;
-            }
-            if (Input.GetKeyUp(KeyCode.LeftAlt))
-            {
-                isAlt = false;
-            }
-            isAlt = true;
-
-            if (Input.GetMouseButton(0))
+            if (Input.GetMouseButton(1))
             {
                 curPoint = Input.mousePosition;
 
-                if (Input.GetMouseButtonDown(0))
+                if (Input.GetMouseButtonDown(1))
                 {
                     prevPoint = Input.mousePosition;
                 }
-
-                if (isAlt)
-                {
-
-                    Vector3 move = (curPoint - prevPoint) * (-1) * dragSpeed;
-                    transform.Translate(move);
-                    if (transform.position.y > -4)
-                        transform.position = new Vector3(transform.position.x, -4, -10);
-                    if (transform.position.y < -24)
-                        transform.position = new Vector3(transform.position.x, -24, -10);
-                    if (transform.position.x < 0)
-                        transform.position = new Vector3(0, transform.position.y, -10);
-                    if (transform.position.x > 28)
-                        transform.position = new Vector3(28, transform.position.y, -10);
-                }
+                Vector3 move = (curPoint - prevPoint) * (-1) * dragSpeed;
+                transform.Translate(move);
+                if (transform.position.y > -4)
+                    transform.position = new Vector3(transform.position.x, -4, -10);
+                if (transform.position.y < -24)
+                    transform.position = new Vector3(transform.position.x, -24, -10);
+                if (transform.position.x < 0)
+                    transform.position = new Vector3(0, transform.position.y, -10);
+                if (transform.position.x > 28)
+                    transform.position = new Vector3(28, transform.position.y, -10);
                 prevPoint = Input.mousePosition;
             }
         }
--- Project_Zero/Assets/Scripts/Ending/Fade in.cs
+++ Project_Zero/Assets/Scripts/Ending/Fade in.cs
@@ -1,16 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Fadein : MonoBehaviour
 {
+    private Color buttonColor;
+    private Image buttonImg;
+    [SerializeField]
+    private int timing;
+    private float fadeIn = 0f;
     private void Start()
     {
-        
+        buttonImg = GetComponent<Image>();
+        buttonColor = buttonImg.color;
     }
 
     private void Update()
     {
-        
+        if (timing > 0)
+        {
+            timing--;
+            return;
+        }
+        buttonColor.a = fadeIn;
+        buttonImg.color = buttonColor;
+        fadeIn += 0.01f;
     }
 }
